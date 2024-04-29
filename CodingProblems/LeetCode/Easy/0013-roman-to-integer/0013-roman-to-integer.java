49c04940cc14fd3c418bedce37a149eadaffc3c7
mumoon14 <118515323+mumoon14@users.noreply.github.com>
2024-04-29 16:14:17 +0900

    Time: 5 ms (58.76%), Space: 44.4 MB (75.82%) - LeetHub

--- CodingProblems/LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
+++ CodingProblems/LeetCode/Easy/0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,5 +1,6 @@
 class Solution {
     public int romanToInt(String s) {
+        int sLen = s.length();
         Map<Character, Integer> map = new HashMap<>();
         map.put('I', 1);
         map.put('V', 5);
@@ -8,19 +9,20 @@ class Solution {
         map.put('C', 100);
         map.put('D', 500);
         map.put('M', 1000);
-        int sLen = s.length();
+        if (sLen == 1) {
+            return map.get(s.charAt(0));
+        }
         int rtnNum = 0;
-        for (int i = 0; i < sLen; i++) {
-            int preNum = map.get(s.charAt(i));
-            if (sLen - 1 == i) {
-                rtnNum += preNum;
-                return rtnNum;
-            }
-            int nextNum = map.get(s.charAt(i + 1));
+        for (int i = 1; i < sLen; i++) {
+            int preNum = map.get(s.charAt(i - 1));
+            int nextNum = map.get(s.charAt(i));
             if (preNum >= nextNum) {
                 rtnNum += preNum;
             } else {
                 rtnNum -= preNum;
+            }
+            if (sLen - 1 == i) {
+                rtnNum += nextNum;
             }
         }
         return rtnNum;
