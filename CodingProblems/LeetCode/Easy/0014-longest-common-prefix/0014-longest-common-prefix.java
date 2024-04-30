67e19b970b09c2b1a0f77ea5791ce868ba025782
mumoon14 <118515323+mumoon14@users.noreply.github.com>
2024-04-30 11:49:12 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ CodingProblems/LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder("");
+        boolean isBreak = false;
+        for (int i = 0; i < strs[0].length(); i++) {
+            sb.append(strs[0].charAt(i));
+            for (String s : strs) {
+                if (s.indexOf(sb.toString()) != 0) {
+                    isBreak = true;
+                    break;
+                }
+            }
+            if (isBreak) break;
+        }
+        if(sb.length() > 0 && strs.length > 1 && isBreak = true) sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
+    }
+}
