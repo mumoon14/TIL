fa50f5751f64c23150c57bdabc534b93685896ad
mumoon14 <118515323+mumoon14@users.noreply.github.com>
2024-04-30 16:41:56 +0900

    Time: 2 ms (54.85%), Space: 41.6 MB (29.98%) - LeetHub

--- CodingProblems/LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ CodingProblems/LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,8 +1,28 @@
 class Solution {
     public boolean isValid(String s) {
-        do {
-            s = s.replace("()", "").replace("[]", "").replace("{}", "");
-        } while (s.indexOf("()") != -1 || s.indexOf("[]") != -1 || s.indexOf("{}") != -1);
-        return s.length() == 0 ? true : false;
+        Stack<Character> stack = new Stack<>();
+
+        for (Character c : s.toCharArray()) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char c2 = stack.peek();
+                if ((c2 == '(' && c == ')') || (c2 == '[' && c == ']') || (c2 == '{' && c == '}')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+        // do {
+        //     s = s.replace("()", "").replace("[]", "").replace("{}", "");
+        // } while (s.indexOf("()") != -1 || s.indexOf("[]") != -1 || s.indexOf("{}") != -1);
+        // return s.length() == 0 ? true : false;
     }
 }
