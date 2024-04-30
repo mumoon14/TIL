744a2c51b4a3111e5c25eaf8daf76311f38279f3
mumoon14 <118515323+mumoon14@users.noreply.github.com>
2024-04-30 17:03:34 +0900

    Time: 2 ms (54.85%), Space: 41 MB (96.16%) - LeetHub

--- CodingProblems/LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
+++ CodingProblems/LeetCode/Easy/0020-valid-parentheses/0020-valid-parentheses.java
@@ -19,10 +19,5 @@ class Solution {
         }
 
         return stack.isEmpty();
-
-        // do {
-        //     s = s.replace("()", "").replace("[]", "").replace("{}", "");
-        // } while (s.indexOf("()") != -1 || s.indexOf("[]") != -1 || s.indexOf("{}") != -1);
-        // return s.length() == 0 ? true : false;
     }
 }
